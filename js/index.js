1602e3d42b3822d41bf83fc9a35ee14f326c60dd
FranciscoJose28 <fcojmatos28@gmail.com>
2024-02-08 12:16:01 -0300

    dia feito

--- js/index.js
+++ js/index.js
@@ -1,10 +1,48 @@
 mes.onchange = () => {
-    switch(mes.value.toLowerCase){
-        case "Janeiro":
+    switch(mes.value.toLowerCase()){
+        case "janeiro":
             nome.innerHTML = "Rei"
         break;
-        case "Fevereiro":
+        case "fevereiro":
             nome.innerHTML = "Mô"
         break;
+        case "março":
+            nome.innerHTML = "Cupido"
+        break;
+        case "abril":
+            nome.innerHTML = "Apaixonado"
+        break;
+        case "maio":
+            nome.innerHTML = "Pagodeiro"
+        break;
+        case "junho":
+            nome.innerHTML = "Cara perfeito"
+        break;
+        case "julho":
+            nome.innerHTML = "Mensageiro"
+        break;
+        case "agosto":
+            nome.innerHTML = "Perturbado"
+        break;
+        case "setembro":
+            nome.innerHTML = "Gueri gueri"
+        break;
+        case "outubro":
+            nome.innerHTML = "Coração"
+        break;
+        case "novembro":
+            nome.innerHTML = "Linda voz"
+        break;
+        case "dezembro":
+            nome.innerHTML = "Criado mudo"
+        break;
+    }
+}
+dia.onchange = () => {
+    if(dia.value >= 1 && dia.value <=3){
+        nome.innerHTML += " que tanto fez ou tanto faz"
+    }
+    if(dia.value >= 4 && dia.value <=6){
+        nome.innerHTML += " que desabou o barraco"
     }
 }
